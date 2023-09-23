2006ba30f3ea3354963eea3affeacf3ff0f72c3d
Reith <can.tej@gmail.com>
2023-09-23 14:57:31 +0530

    Added function to create a database for keeping playing scores

--- SqlHelper.py
+++ SqlHelper.py
@@ -1,13 +1,23 @@
-def f(databasename, tablename,username,password,tuple):
+def f(databasename, tablename, username, password, tuple):
     import mysql.connector as s
+
     try:
-        m=s.connect(host='localhost',username='root',password='Harshan@4506',database='{}').format(databasename)
+        m = s.connect(
+            host="localhost",
+            username="root",
+            password="Harshan@4506",
+            database="{}",
+        ).format(databasename)
     except Exception:
-        m=s.connect(host='localhost',username='root',password='Harshan@4506')
-        w=m.cursor()
+        m = s.connect(
+            host="localhost", username="root", password="Harshan@4506"
+        )
+        w = m.cursor()
         w.execute('create database "{}"').format(databasename)
     w.execute("use databasename")
     w.execute("show tables")
-    q=w.fetchall()
+    q = w.fetchall()
     if tablename not in q:
-        w.execute('create table "{}"(Id varchar(50),Player_Name varchar(50),Score int').format(tablename)
+        w.execute(
+            'create table "{}"(Id varchar(50),Player_Name varchar(50),Score int'
+        ).format(tablename)
