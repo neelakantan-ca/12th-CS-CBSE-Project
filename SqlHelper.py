ec4fdc952ec641fd7072a29c75684664d534f5d1
Blackdevil4506 <harshavardhaan45@gmail.com>
2023-09-24 19:17:50 +0530

    wrote func to create database&table if not present

---
+++ SqlHelper.py
@@ -0,0 +1,13 @@
+def f(databasename, tablename,username,password,tuple):
+    import mysql.connector as s
+    try:
+        m=s.connect(host='localhost',username='root',password='Harshan@4506',database='{}').format(databasename)
+    except Exception:
+        m=s.connect(host='localhost',username='root',password='Harshan@4506')
+        w=m.cursor()
+        w.execute('create database "{}"').format(databasename)
+    w.execute("use databasename")
+    w.execute("show tables")
+    q=w.fetchall()
+    if tablename not in q:
+        w.execute('create table "{}"(Id varchar(50),Player_Name varchar(50),Score int').format(tablename)
